9ff96e88f3f53f512654868a32372959085d6d16
Shreyas-Patil-11 <shreyaspatil1311@gmail.com>
2024-10-07 23:39:03 +0530

    path changed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-export { default as useDataFetch } from './useDataFetch';
-export { default as useDebounce } from './useDebounce';
-export { default as useLocalStorage } from './useLocalStorage';
-export { default as usePrevious } from './usePrevious';
+export { default as useDataFetch } from './hooks/useDataFetch';
+export { default as useDebounce } from './hooks/useDebounce';
+export { default as useLocalStorage } from './hooks/useLocalStorage';
+export { default as usePrevious } from './hooks/usePrevious';
